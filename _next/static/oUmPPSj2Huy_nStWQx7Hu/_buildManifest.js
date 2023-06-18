f5eb55d759e033b1c2c3bc55d5c2586ebbf3fc1a
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2023-06-18 14:31:01 +0000

    moving files

---
+++ _next/static/oUmPPSj2Huy_nStWQx7Hu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/19-c623ef75ba9577f7.js","static/chunks/pages/index-d070c3470d718d11.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
